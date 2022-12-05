8360ae89fd3f6be8e8fa9608cee136a6a289b9cd
Ikeda Megumi <megumi9958@gmail.com>
2022-12-05 09:54:57 +0900

    管理者用画面にボタンを追加

--- pbl2022/src/pb_home.php
+++ pbl2022/src/pb_home.php
@@ -1,3 +1,11 @@
 <div class="pb_home">
   <h2>ホーム画面</h2>
+    <?php
+      if (isset($_SESSION['usertype_id'])){
+        if ($_SESSION['usertype_id']==='9'){  //管理者
+          echo '<button type="button">管理者情報変更</buton>';
+          echo '<button type="button">アカウント一覧表示</buton>';
+        }
+      }
+    ?>
 </div>
--- pbl2022/src/usr_detail.php
+++ pbl2022/src/usr_detail.php
@@ -7,14 +7,19 @@ $sql = "SELECT * FROM t_user WHERE user_id='{$uid}'";
 $rs = $conn->query($sql);
 if (!$rs) die('エラー: ' . $conn->error);
 
+echo '<h2>アカウント情報</h2>';
+
 echo '<table border=1>';
 $row= $rs->fetch_assoc();
 if ($row) {
- echo '<tr><th>ユーザID</th><td>' . $row['user_id'] . '</td></tr>';
- echo '<tr><th>ユーザ名</th><td>' . $row['user_name']. '</td></tr>';
+ echo '<tr><th>ID</th><td>' . $row['user_id'] . '</td></tr>';
+ echo '<tr><th>アカウント名</th><td>' . $row['user_name']. '</td></tr>';
+ echo '<tr><th>本名</th><td><input type="text" value="' . $row['user_name']. '"></td></tr>';
  $codes = array('1'=>'会員', '2'=>'ゲスト', '9'=>'管理者' );
  $i  = $row['usertype_id'];     // 数字のユーザ種別を取得
  echo '<tr><th>ユーザ種別</th><td>' . $codes[$i]. '</td></tr>';
 }
 echo '</table>';
+echo '<button type="button">パスワード初期化</button>';
+echo '<button type="button">決定</button>';
 ?>
